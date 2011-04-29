6ee5bc926833f064110c8edf38f1317e4682a8b5
khayashi <khermitian@gmail.com>
2011-04-29 11:53:41 -0500

    fixed a lot of bugs in the twitter api part

--- scraper.py
+++ scraper.py
@@ -2,51 +2,75 @@ import twitter
 from config import Config
 
 ############################
-#     Helper Functions     #
+#       Configuration      #
 ############################  
 
 f = file('myconfig.cfg')
 cfg = Config(f)
 api = twitter.Api(consumer_key = cfg.oAuth[0].consumer_key, 
-                  consumer_secret = cfg.oAuth[0].consumer_key,
-                  access_token_key = cfg.oAuth[0].consumer_key,
-                  access_token_secret= cfg.oAuth[0].consumer_key)
+                  consumer_secret = cfg.oAuth[0].consumer_secret,
+                  access_token_key = cfg.oAuth[0].access_token,
+                  access_token_secret= cfg.oAuth[0].access_token_secret)
+
+#just checking that I logged in...
+print api.VerifyCredentials() 
                   
 ############################
 #     Helper Functions     #
 ############################  
 
-def getUsers(users):
-    for user in users:
-        if len(users) < MAX_QUERIES:
-            users += api.GetFriends(user = user)
-        else:
-            return users
+#MAX_QUERIES = 349/2
+# Use this for testing
+MAX_QUERIES = 50/2
+MAX_TWEETS = 190
 
+
+def getUsers(users):
+    i = 0
+    print len(users)
+    while len(users) < MAX_QUERIES:
+        user = users[i]
+        print 'here ' + str(i) 
+        i += 1
+        users.extend( api.GetFriends(user = user.id)[:10] )#only grab first 10 friends
+    return users
+
+def filterUsers(users):
+    for user in users[:]:
+        if user.GetLang() != 'en':
+            users.remove(user)
+        elif user.GetProtected():
+            users.remove(user)
     return users
 
 def getTweets(users):
     tweets = {}
     for user in users:
-        tweets[str(user)] = api.GetUserTimeline(user, count = MAX_TWEETS)
+        if user.GetProtected():
+            print 'oh no !'
+        try:
+            tweets[str(user.id)] = api.GetUserTimeline(user.id, count = MAX_TWEETS)
+        except:
+            continue
     return tweets
 
 ################
 #     Main     #
 ################  
 
-#MAX_QUERIES = 349/2
-# Use this for sexting
-MAX_QUERIES = 10/2
-MAX_TWEETS = 200
-
 
 initTweets = api.GetPublicTimeline()
-users = getUsers([s.user.id for s in initTweets])
-tweets = getTweets(users)
+#users = getUsers([s.user for s in initTweets])
+
+tweets = getTweets( filterUsers( getUsers( filterUsers([s.user for s in initTweets]))))
+
 # Write some LEET shiet to load into the database
+print len(tweets)
 for u, t in tweets.iteritems():
-    print t[0].text
+    if len(t) > 0:
+        print 'number of tweets: %s; 1st tweet: %s' % (len(t), t[0].text)
+    else:
+        print 'null tweet'
     
 
 
