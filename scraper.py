3618731c52aa14a6d180c8d982c8eec3e5def457
Daniel <daniel@dandroid.(none)>
2011-05-01 15:59:52 -0500

    refactored some code

--- scraper.py
+++ scraper.py
@@ -24,7 +24,7 @@ api = twitter.Api(consumer_key = cfg.oAuth[0].consumer_key,
 
 MAX_QUERIES = 300/2
 # Use this for testing
-#MAX_QUERIES = 10/2
+#MAX_QUERIES = 5/2
 MAX_TWEETS = 190
 DB = 'tweets.db'
 
@@ -58,10 +58,39 @@ def printUsersInfo(users):
     # I am assuming at some point we will want a fancier print function
     print 'Number of Valid Users', len(users)
 
+def formatUser(user):
+    return (unicode(user.GetId()),
+              '\'' + unicode(user.GetName()) + '\'',
+              '\'' + unicode(user.GetCreatedAt()) + '\'', 
+              '\'' + unicode(user.GetLocation()) + '\'', 
+              '\'' + unicode(user.GetDescription()) + '\'', 
+              unicode(int(user.GetGeoEnabled())),
+              unicode(user.GetFriendsCount()),
+              unicode(user.GetStatusesCount()), 
+              '\'' + unicode(user.GetScreenName()) + '\'',
+              '\'' + unicode(user.GetTimeZone()) + '\'', 
+              '\'' + unicode(user.GetUrl()) + '\'',
+              unicode(user.GetUtcOffset()), 
+              '\'' + unicode(user.GetLang()) + '\'')
 
-
-
-
+def formatTweet(tweet):
+    return (unicode(tweet.GetId()),
+              '\'' + unicode(tweet.GetCreatedAt()) + '\'',
+              unicode(tweet.GetUser()),
+              unicode(int(tweet.GetFavorited())),
+              '\'' + unicode(tweet.GetInReplyToScreenName()) + '\'',
+              unicode(tweet.GetInReplyToUserId()),
+              unicode(tweet.GetInReplyToStatusId()),
+              unicode(int(tweet.GetTruncated())),
+              '\'' + unicode(tweet.GetSource()) + '\'',
+              '\'' + unicode(tweet.GetText()) + '\'',
+              '\'' + unicode(tweet.GetLocation()) + '\'',
+              '\'' + unicode(tweet.GetRelativeCreatedAt()) + '\'',
+              '\'' + unicode(tweet.GetUser()) + '\'',
+              '\'' + unicode(tweet.GetGeo()) + '\'',
+              '\'' + unicode(tweet.GetPlace()) + '\'',
+              '\'' + unicode(tweet.GetCoordinates()) + '\'')
+              
 
 #######################
 #     Grab Tweets     #
@@ -91,39 +120,13 @@ connection = sqlite.connect(DB)
 cursor = connection.cursor()
 
 for user in users:
-    values = (unicode(user.GetId()),
-              '\'' + unicode(user.GetName()) + '\'',
-              '\'' + unicode(user.GetCreatedAt()) + '\'', 
-              '\'' + unicode(user.GetLocation()) + '\'', 
-              '\'' + unicode(user.GetDescription()) + '\'', 
-              unicode(int(user.GetGeoEnabled())),
-              unicode(user.GetFriendsCount()),
-              unicode(user.GetStatusesCount()), 
-              '\'' + unicode(user.GetScreenName()) + '\'',
-              '\'' + unicode(user.GetTimeZone()) + '\'', 
-              '\'' + unicode(user.GetUrl()) + '\'',
-              unicode(user.GetUtcOffset()), 
-              '\'' + unicode(user.GetLang()) + '\'')
+    values = formatUser(user)
     cursor.execute('insert into users values (?,?,?,?,?,?,?,?,?,?,?,?,?)', values)
     connection.commit()
 
 for tweet in tweets:
-    values = (unicode(tweet.GetId()),
-              '\'' + unicode(tweet.GetCreatedAt()) + '\'',
-              unicode(tweet.GetUser()),
-              unicode(int(tweet.GetFavorited())),
-              '\'' + unicode(tweet.GetInReplyToScreenName()) + '\'',
-              unicode(tweet.GetInReplyToUserId()),
-              unicode(tweet.GetInReplyToStatusId()),
-              unicode(int(tweet.GetTruncated())),
-              '\'' + unicode(tweet.GetSource()) + '\'',
-              '\'' + unicode(tweet.GetText()) + '\'',
-              '\'' + unicode(tweet.GetLocation()) + '\'',
-              '\'' + unicode(tweet.GetRelativeCreatedAt()) + '\'',
-              '\'' + unicode(tweet.GetUser()) + '\'',
-              '\'' + unicode(tweet.GetGeo()) + '\'',
-              '\'' + unicode(tweet.GetPlace()) + '\'',
-              '\'' + unicode(tweet.GetCoordinates()) + '\'')
+    values = formatTweet(tweet)
     cursor.execute('insert into status values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', values)
     connection.commit()
 
+print "\n"
