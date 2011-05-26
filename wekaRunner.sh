11c8fe264ebaede216ae7eea4e8217b4e3f099e2
Daniel <daniel@dandroid.(none)>
2011-05-26 15:43:08 -0500

    fixed kai's retard script :-p

--- wekaRunner.sh
+++ wekaRunner.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
-FULL_SET = seomset.arff
-USER_SET = userset.arff
-LA_NY_SET = nyla.arff
+FULL_SET=1234.arff
+USER_SET=1234.arff
+LA_NY_SET=1234.arff
 
-Set[1] = $FULL_SET
-Set[2] = $USER_SET
-Set[3] = $LA_NY_SET
+Set[1]=$FULL_SET
+Set[2]=$USER_SET
+Set[3]=$LA_NY_SET
 
 for i in 1 2 3
 do
-    OUT = `echo ${Set[i]} | sed s/ "\.arff"//g`
+    OUT=`echo ${Set[i]} | sed 's/\(.*\)\..*/\1/'`
+    echo $OUT
     java -Xmx24G -cp weka.jar weka.classifiers.bayes.NaiveBayes -t ${Set[i]} -x 10 -i -k -c 1 > "$OUT-results.out"
 done
